01d2a7327c57618964b3fc30a3ba202de973f9f6
Chiaki.C <akiakira02@gmail.com>
2026-07-11 12:25:38 +0800

    fix: handle potential null value in glyph display character retrieval

--- src/features/editor/canvas/CanvasWorkspace.tsx
+++ src/features/editor/canvas/CanvasWorkspace.tsx
@@ -197,7 +197,7 @@ export function CanvasWorkspace() {
       }
       const glyph = fontData?.glyphs[glyphId]
       setEditorTextState(
-        glyph ? getGlyphDisplayCharacter(glyph) : '',
+        glyph ? (getGlyphDisplayCharacter(glyph) ?? '') : '',
         [glyphId],
         1,
         0
